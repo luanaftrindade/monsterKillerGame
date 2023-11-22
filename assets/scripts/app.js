9c005a7c271bb06b29ef23b282f67cd039ffa068
luanaftrindade <luanatrindade.dev@gmail.com>
2023-11-22 13:40:26 +0000

    add new functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,63 @@
 let chosenMaxLife = 100;
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 15;
 const MONSTER_ATTACK_VALUE = 10;
+const HEAL_VALUE = 14;
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
-function attackHandler(){
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === "ATTACK") {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === "STRONG_ATTACK_VALUE") {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
+    endRound();
+
+}
+
+function endRound() {
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
-    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert("You won!");
-    } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0){
+    } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
         alert("You lost!");
-    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0){
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
         alert("It's a draw...");
     }
 
 }
 
+function attackHandler() {
+    attackMonster("ATTACK");
+
+}
+
+function strongAttackHandler() {
+    attackMonster("STRONG_ATTACK_VALUE");
+
+}
+
+function healHandler() {
+    increasePlayerHealth(HEAL_VALUE);
+    endRound();
+
+}
+
+function logHandler() {
+
+
+}
+
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healHandler);
+logBtn.addEventListener('click', logHandler);
