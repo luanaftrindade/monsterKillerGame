1c78eb7d00e3accf484b39a5f25ab6cc5766298d
luanaftrindade <luanatrindade.dev@gmail.com>
2023-11-25 22:03:21 +0000

    css

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,7 @@ const HEAL_VALUE = 14;
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 
 function attackMonster(mode) {
@@ -19,18 +20,43 @@ function attackMonster(mode) {
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
     endRound();
-
 }
+
+
+function reset() {
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
+
 
 function endRound() {
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
+    
+if(currentPlayerHealth <= 0 && hasBonusLife){
+
+    hasBonusLife = false;
+    removeBonusLife();
+    reset();
+    setPlayerHealth(initialPlayerHealth);
+    alert("The bonus life save you.");
+    
+}
+
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert("You won!");
+     
     } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
         alert("You lost!");
+       
     } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
         alert("It's a draw...");
+      
+    }
+
+    if(currentMonsterHealth <= 0 || currentPlayerHealth <= 0){
+        reset();
     }
 
 }
@@ -46,7 +72,15 @@ function strongAttackHandler() {
 }
 
 function healHandler() {
-    increasePlayerHealth(HEAL_VALUE);
+    let healValue;
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE){
+        alert('You could not heal more than your max life!');
+        healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
     endRound();
 
 }
